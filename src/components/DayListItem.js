99cf68f44df1cd2b50172a322353bba2009b1a44
Hammad Seehar <hammad96@outlook.com>
2020-04-14 10:27:06 -0400

    Finished day 1 of react

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classnames from 'classnames';
+import './DayListItem.scss';
+
+export default function DayListItem(props) {
+
+  const dayClass = classnames({
+    'day-list_item': true,
+    'day-list_item--selected': props.selected,
+    'day-list_item--full': props.spots === 0
+
+
+
+
+  });
+
+  const formatSpot = spots => {
+    switch (spots) {
+      case 0:
+        return "no spots remaining";
+      case 1:
+        return "1 spot remaining";
+
+      default:
+        return `${spots} spots remaining`;
+    }
+  };
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpot(props.spots)}</h3>
+    </li>
+  );
+}
